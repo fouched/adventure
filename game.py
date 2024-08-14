55241ced03def1c0db2744ba17053276f0b1e328
Fouche du Preez <fouche@limehouse.co.za>
2024-08-14 22:07:11 +0200

    added help and colours

--- game.py
+++ game.py
@@ -1,6 +1,9 @@
+from colorama import Fore
+
+
 def welcome():
-    print("                                                  DUNGEON")
-    print("""
+    print(Fore.RED + "                                                  D U N G E O N")
+    print(Fore.GREEN + """
     The village of Honeywood has been terrorized by strange, deadly creatures for months now. Unable to endure any 
     longer, the villagers pooled their wealth and hired the most skilled adventurer they could find: you. After
     listening to their tale of woe, you agree to enter the labyrinth where most of the creatures seem to originate,
@@ -19,11 +22,11 @@ def play_game():
 
 def explore_labyrinth():
     while True:
-        player_input = input("-> ")
+        player_input = input(Fore.YELLOW + "-> ").lower().strip()
 
         # process input
         if player_input == "help":
-            print("Show help")
+            show_help()
 
         elif player_input == "quit":
             print("Overcome with terror, you flee the dungeon.")
@@ -35,9 +38,40 @@ def explore_labyrinth():
 
 
 def play_again():
-    yn = input("Play again? (yes/no) -> ")
+    yn = get_yn(Fore.CYAN + "Play again")
     if yn == "yes":
         play_game()
     else:
         print("Until next time, adventurer.")
         exit(0)
+
+
+def get_yn(question: str) -> str:
+    while True:
+        answer = input(question + " (yes/no) -> ").lower().strip()
+        if answer not in ["yes", "no", "y", "n"]:
+            print("Please enter yes or no.")
+        else:
+            if answer == "y":
+                answer = "yes"
+            elif answer == "n":
+                answer = "no"
+
+            return answer
+
+
+def show_help():
+    print(Fore.GREEN + """Enter a command:
+    - n/s/e/w : move in a direction
+    - map : show a map of the labyrinth
+    - look : look around and describe you environment
+    - equip <item> : use an item from your inventory
+    - unequip <item> : stop using an item from your inventory
+    - fight : attack a foe
+    - examine <object> : examine an object more closely
+    - get <item> : pick up an item
+    - drop <item> : drop an item
+    - rest : restore health by resting
+    - inventory : show current inventory
+    - status : show current player status
+    - quit : end the game""")
--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import game
 
+from blessed import Terminal
+
 
 def main():
+    # clear terminal
+    term = Terminal()
+    print(term.clear())
+
     # play game until game over or player quits
     game.play_game()
 
